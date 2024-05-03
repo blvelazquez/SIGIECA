962f49fd17c075c841011bf1fcb47ee6e650887e
JoseBaldor <jfco37237@gmail.com>
2024-05-02 21:58:24 -0600

    cotroller y model

---
+++ app/models/usuariosModel.php
@@ -0,0 +1,21 @@
+<?php
+        
+    class usuariosModel{
+        private $PDO;
+
+        public function __construct() {
+            require_once(__DIR__ . "/../config/db.php");
+            $con = new db_local();
+            $this->PDO = $con->conexion();
+        }
+
+        public function insert($login, $password) {
+            $stament = $this->PDO->prepare("INSERT INTO sig_usuarios (loginUsuario, passwordUsuario) 
+            VALUES (:login, :password)");
+            $stament->bindParam(":login", $login, PDO::PARAM_STR);
+            $stament->bindParam(":password", $password, PDO::PARAM_STR);
+            return ($stament->execute()) ? $this->PDO->lastInsertId() : false ;
+        }
+       
+    }
+?>
