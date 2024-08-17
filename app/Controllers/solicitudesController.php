b75391bbb2c665aa4ee9f3d81d6b001d7d18567e
JoseBaldor <jfco37237@gmail.com>
2024-08-16 22:03:33 -0600

    solicitudes

--- app/Controllers/solicitudesController.php
+++ app/Controllers/solicitudesController.php
@@ -17,9 +17,34 @@
 
         public function save($idUsuario, $idEspacio, $idPartida, $idProceso, $descripcion, $tipo){
             // metodo para obtener el id de la solicitud
+            $tipos = [
+                '1' => 'SCO',
+                '2' => 'SMI',
+                '3' => 'SME'
+            ];
+            
+            // Asignación con un valor por defecto
+            $tipoSolicitud = $tipos[$tipo] ?? 'S';
+            echo("<br>".$tipoSolicitud);
+
+            $row = $this->model->last($tipo);
+            
+            // if (!empty($row) && isset($row[0]['total'])) {
+            //     $cons = $row[0]['total'];
+            //     echo "<br> Este es el último: " . $cons;
+            // } else {
+            //     $cons='001';
+            //     echo("<br> No se encontró ningún registro.".$cons);
+            // }
+            
+            $cons = str_pad($row[0]['total'] + 1, 3, '0', STR_PAD_LEFT); // Asegura que tenga 3 dígitos, con ceros a la izquierda
+
+            // echo("<br>".$cons);
+            $añoActual = date('y'); // Obtiene los últimos dos dígitos del año actual
+            
             $fecha_solicitud = date("y-m-d");
-            $id = $this->model->insert('S-ACA-003-24', $idUsuario, $idPartida, $idProceso, $fecha_solicitud, $tipo, $descripcion, $idEspacio);
-            return ($id!= false) ? header($this->location) : false;
+            // $id = $this->model->insert('SCO-ACA-002-24', $idUsuario, $idPartida, $idProceso, $fecha_solicitud, $tipo, $descripcion, $idEspacio);
+            // return ($id!= false) ? header($this->location) : false;
         }        
     }
 ?>
