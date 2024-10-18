7f1d8add65504dafc99da82545300d23a6c1b2d0
JoseBaldor <jfco37237@gmail.com>
2024-10-17 22:01:58 -0600

    carga de archivos

--- app/Controllers/solicitudesController.php
+++ app/Controllers/solicitudesController.php
@@ -5,12 +5,58 @@
     class solicitudesController{
         private $model;
         private $location;
+        private $fileErrors;
+        private $dir;
 
         public function __construct(){
             $this->model = new solicitudesModel();
+            $this->fileErrors = "errors.log";
+            $this->dir = "uploads";
             $this->location = "Location:../../../index.php#views/solicitudes/listaSolicitudes";
         }
 
+        public function saveFile($id, $typeMessage){
+            if($id != false){
+                
+                $messageError = ($typeMessage == 'SAVE') ? "Registro Guardado con Éxito" : "Registro Actualizado con éxito";
+                file_put_contents($this->fileErrors, 'INFO '.$messageError.PHP_EOL , FILE_APPEND);
+                
+                if($_FILES['upfile']['error'] == UPLOAD_ERR_OK){
+                    // Cambiar el formato permitido a PDF
+                    $allowedFormat = array("application/pdf");
+                    
+                    // Verificar si el archivo subido es un PDF
+                    if(in_array($_FILES['upfile']['type'], $allowedFormat)){
+                        
+                        $info_file = pathinfo($_FILES['upfile']['name']);
+                        // Crear la ruta con la extensión PDF
+                        $file = $this->dir.'/'.$id.".pdf";
+        
+                        // Verificar si la carpeta de destino existe, si no, crearla
+                        if(!file_exists($this->dir)){
+                            mkdir($this->dir, 0777);
+                            chmod($this->dir, 0755);
+                        }
+        
+                        // Mover el archivo subido a la carpeta de destino
+                        if(!move_uploaded_file($_FILES['upfile']['tmp_name'], $file)){
+                            unlink($this->fileErrors);
+                            $messageError = ($typeMessage == 'SAVE') ? "Error al Guardar el Archivo" : "Error al Actualizar el Archivo";
+                            file_put_contents($this->fileErrors, 'ERROR '.$messageError.PHP_EOL , FILE_APPEND);
+                        }
+        
+                    }else{
+                        unlink($this->fileErrors);
+                        $messageError = "Formato de Archivo no Permitido (Solo PDF)";
+                        file_put_contents($this->fileErrors, 'ERROR '.$messageError.PHP_EOL , FILE_APPEND);
+                    }
+                }
+            }else{
+                $messageError = ($typeMessage == 'SAVE') ? "Error al Guardar el Registro" : "Error al Actualizar el Registro";                        
+                file_put_contents($this->fileErrors, 'ERROR '.$messageError.PHP_EOL , FILE_APPEND);
+            }
+        }
+        
         public function list(){
             return ($this->model->list()) ? $this->model->list() : false;
         }
@@ -49,6 +95,7 @@
             $idSolicitud = $tipoSolicitud.'-'.$ini[0]['iniciales'].'-'.$cons.'-'.$añoActual;
 
             $id = $this->model->insert($idSolicitud, $idUsuario, $idPartida, $idProceso, $fecha_solicitud, $tipo, $descripcion, $idEspacio);
+            $this->saveFile($id, 'SAVE');
             return ($id!= false) ? header($this->location) : false;
         }        
     }
